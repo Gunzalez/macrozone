642d4af09f723816e114ae6dc466be7bad40cc5c
Segun Konibire <gunzalez@hotmail.com>
2026-05-03 20:41:11 +0100

    Implement meal loading functionality in HomeScreen and update RecentMeals component to display fetched meals

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,32 @@
+import { getMeals, Meal } from "@/storage/meals";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
 import { ScrollView, Text } from "react-native";
-import HeaderBar from "../components/HeaderBar";
+import HomeHeader from "../components/HeaderBar";
 import MacroGrid from "../components/MarcoGrid";
 import RecentMeals from "../components/RecentMeals";
 import { globalStyles } from "../styles/global";
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
-      <HeaderBar />
+      <HomeHeader />
       <MacroGrid />
-      <RecentMeals />
+      <RecentMeals meals={meals} />
     </ScrollView>
   );
 }
