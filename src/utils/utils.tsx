9092810a0e866300ef9d21afcfb475e04be2bc28
Segun Konibire <gunzalez@hotmail.com>
2026-05-04 12:27:30 +0100

    Add getTotals utility function to calculate total nutritional values from an array of meals.

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,3 +1,17 @@
+import { Meal } from "@/storage/meals";
+
 export function prepareValue(value: string): number {
   return Math.abs(Number(value)) || 0;
+}
+
+export function getTotals({ meals }: { meals: Meal[] }) {
+  return meals.reduce(
+    (acc, meal) => ({
+      calories: acc.calories + meal.calories,
+      protein: acc.protein + meal.protein,
+      carbs: acc.carbs + meal.carbs,
+      fat: acc.fat + meal.fat,
+    }),
+    { calories: 0, protein: 0, carbs: 0, fat: 0 },
+  );
 }
